e94b1e22dce1cc509cd5e45b2c69c0926b5578f8
Marcos Ardissino <m.ardissino@scientia.com.ar>
2022-06-23 14:20:45 -0300

    probando guardar evento install pwa

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ if ('serviceWorker' in navigator) {
     .then(reg => console.log('Registro de SW exitoso', reg))
     .catch(err => console.warn('Error al tratar de registrar el sw', err))
 }
+
+// Inicializa deferredPrompt para su uso más tarde.
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Previene a la mini barra de información que aparezca en smartphones
+  e.preventDefault();
+  // Guarda el evento para que se dispare más tarde
+  deferredPrompt = e;
+  // Actualizar la IU para notificarle al usuario que se puede instalar tu PWA
+  showInstallPromotion();
+  // De manera opcional, envía el evento de analíticos para saber si se mostró la promoción a a instalación del PWA
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
